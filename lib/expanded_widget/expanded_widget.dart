3486a513607b6a3fe36670d75f4986de3b9293f9
ThanhTan <thanhtan27032001@gmail.com>
2024-03-01 16:53:11 +0700

    [v1.0.0] Expanded widget

---
+++ lib/expanded_widget/expanded_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ExpandedWidget extends StatelessWidget {
+  const ExpandedWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
